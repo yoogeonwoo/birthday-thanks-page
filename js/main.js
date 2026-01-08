80070e5a3f151e9440376ad82e1fa8fd46405aee
geonwoo_yoo <ygw090108@icloud.com>
2026-01-08 13:19:17 +0900

    16

--- js/main.js
+++ js/main.js
@@ -65,7 +65,7 @@ btnOpenLetter.addEventListener("click", () => {
     const sc = document.querySelector("#modalLetter .paperScroll");
     if (sc) sc.scrollTop = 0;
     isAnimating = false;
-  }, 860);
+  }, 900);
 });
 
 btnOpenInbox.addEventListener("click", async () => {
